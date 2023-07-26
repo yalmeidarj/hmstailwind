f31a787e43d4d1340b247f16aa5c3a5363ebe43d
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2023-07-26 09:25:29 -0400

    statusAttempt and consent fix

--- src/app/locations/streets/[...id]/page.tsx
+++ src/app/locations/streets/[...id]/page.tsx
@@ -22,30 +22,37 @@ async function getAllStreetNumbersOfStreet(streetId: any) {
 }
 
 
-function getConditionalClass(statusAttempt: string) {
+function getConditionalClass(statusAttempt: string, consent: string) {
     switch (statusAttempt) {
-        case 'not started':
+        case 'No Attempt':
             return 'bg-gray-300 text-gray-800';
-        case '1st attempt':
+        case 'Door Knock Attempt 1':
             return 'bg-blue-200 text-blue-900';
-        case '2nd attempt':
+        case 'Door Knock Attempt 2':
             return 'bg-yellow-200 text-yellow-900';
-        case '3rd attempt':
+        case 'Door Knock Attempt 3':
             return 'bg-indigo-200 text-indigo-900';
-        case '4th attempt':
+        case 'Door Knock Attempt 4':
             return 'bg-orange-200 text-orange-900';
-        case '5th attempt':
+        case 'Door Knock Attempt 5':
             return 'bg-purple-200 text-purple-900';
-        case 'consent final yes':
-            return 'bg-green-200 text-green-900';
-        case 'consent final no':
+        case 'Door Knock Attempt 6':
             return 'bg-red-200 text-red-900';
+        case 'consent final':
+            if (consent.toLowerCase() == 'yes') {
+                return 'bg-green-200 text-green-900';
+            } else if (consent.toLowerCase() == 'no') {
+                return 'bg-red-200 text-red-900';
+            } else {
+                return 'bg-gray-200 text-gray-900';
+            }
         case 'engineer visit required':
             return 'bg-teal-200 text-teal-900';
         default:
             return 'border-2 border-black text-blue-900';
     }
 }
+
 
 
 
@@ -73,7 +80,7 @@ export default async function Page({
                     // const statusAttempt = house.statusAttempt || '';
                     <Suspense fallback={<p className='text-black'>Loading</p>}>
                         <div key={house.id} className={`w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2`}>
-                            <Link href={`/locations/streets/house/${house.id}`} className={`w-full h-full flex flex-col items-start justify-between py-4 px-6 rounded-lg shadow-md transition-all duration-200 ease-in-out hover:opacity-70 ${getConditionalClass(house?.statusAttempt ?? " ")}`}>
+                            <Link href={`/locations/streets/house/${house.id}`} className={`w-full h-full flex flex-col items-start justify-between py-4 px-6 rounded-lg shadow-md transition-all duration-200 ease-in-out hover:opacity-70 ${getConditionalClass(house?.statusAttempt ?? " ", house?.consent ?? " ")}`}>
                                 <h1 className="text-lg sm:text-xl text-center items-center p-2 md:text-2xl lg:text-3xl font-bold leading-tight mb-2">{house.streetNumber}</h1>
                                 <div className="flex flex-row w-full justify-around border-t border-b border-white py-2 mb-2">
                                     <div className='flex flex-col justify-center items-center border-r p-2 border-white'>
